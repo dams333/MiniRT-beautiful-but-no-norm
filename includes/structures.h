edb11628543ea2ee83d5aec9d1e82f750555d01d
dhubleur <dhubleur@student.42.fr>
2022-05-17 12:01:25 +0200

    structures

---
+++ includes/structures.h
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structures.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/17 11:43:56 by dhubleur          #+#    #+#             */
+/*   Updated: 2022/05/17 11:54:15 by dhubleur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTURES_H
+# define STRUCTURES_H
+
+typedef enum e_object_type
+{
+	CAMERA,
+	AMBIENT_LIGHTNING,
+	LIGHT,
+	SPHERE,
+	PLANE,
+	CYLINDER
+}	t_object_type;
+
+typedef struct s_generic_object
+{
+	t_object_type	type;
+	void			*specific_object;
+}	t_generic_object;
+
+typedef struct s_ambient_lightning_object
+{
+	float	lightning_ratio;
+	int		color_r;
+	int		color_g;
+	int		color_b;
+}	t_ambient_lightning_object;
+
+typedef struct s_camera_object
+{
+	float	coord_x;
+	float	coord_y;
+	float	coord_z;
+	float	orientation_x;
+	float	orientationy;
+	float	orientation_z;
+	int		horizontal_fov;
+}	t_camera_object;
+
+typedef struct s_light_object
+{
+	float	coord_x;
+	float	coord_y;
+	float	coord_z;
+	float	brightness_ratio;
+	int		color_r;
+	int		color_g;
+	int		color_b;
+}	t_light_object;
+
+typedef struct s_sphere_object
+{
+	float	coord_x;
+	float	coord_y;
+	float	coord_z;
+	float	diameter;
+	int		color_r;
+	int		color_g;
+	int		color_b;
+}	t_sphere_object;
+
+typedef struct s_plane_object
+{
+	float	coord_x;
+	float	coord_y;
+	float	coord_z;
+	float	orientation_x;
+	float	orientationy;
+	float	orientation_z;
+	int		color_r;
+	int		color_g;
+	int		color_b;
+}	t_plane_object;
+
+typedef struct s_cylinder_object
+{
+	float	coord_x;
+	float	coord_y;
+	float	coord_z;
+	float	orientation_x;
+	float	orientationy;
+	float	orientation_z;
+	float	diameter;
+	float	hieght;
+	int		color_r;
+	int		color_g;
+	int		color_b;
+}	t_cylinder_object;
+
+#endif
--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 17:44:43 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/05/16 18:26:45 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/05/17 12:00:49 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,70 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-void	parse_line(char *line)
+int	parse_line(char *line)
 {
-	
+	char	**elem;
+
+	elem = ft_split(line, ' ');
+	if (!elem)
+	{
+		ft_putendl_fd("Error\nA malloc failed during parsing", 2);
+		return (1);
+	}
+	if (!elem[0])
+		return (0);
+	if (ft_strlen(elem[0]) == 1)
+	{
+		if(ft_strncmp(elem[0], "C", 1) == 0)
+		{
+
+		}
+		else if(ft_strncmp(elem[0], "A", 1) == 0)
+		{
+
+		}
+		else if(ft_strncmp(elem[0], "L", 1) == 0)
+		{
+			
+		}
+		else
+		{
+			ft_putstr_fd("Error\nObject type ", 2);
+			ft_putstr_fd(elem[0], 2);
+			ft_putendl_fd(" unknown", 2);
+			return (1);
+		}
+	}
+	else if (ft_strlen(elem[0]) == 2)
+	{
+		if(ft_strncmp(elem[0], "sp", 2) == 0)
+		{
+
+		}
+		else if(ft_strncmp(elem[0], "pl", 2) == 0)
+		{
+
+		}
+		else if(ft_strncmp(elem[0], "cy", 2) == 0)
+		{
+			
+		}
+		else
+		{
+			ft_putstr_fd("Error\nObject type ", 2);
+			ft_putstr_fd(elem[0], 2);
+			ft_putendl_fd(" unknown", 2);
+			return (1);
+		}
+	}
+	else
+	{
+		ft_putstr_fd("Error\nObject type ", 2);
+		ft_putstr_fd(elem[0], 2);
+		ft_putendl_fd(" unknown", 2);
+		return (1);
+	}
+	return (0);
 }
 
 int	read_file(int fd)
@@ -28,7 +89,10 @@ int	read_file(int fd)
 	while (line != NULL)
 	{
 		if (ft_strlen(line) > 1)
-			parse_line(line);
+		{
+			if(parse_line(line))
+				return (1);
+		}
 		free(line);
 		res = get_next_line(fd, &line);
 	}
@@ -48,7 +112,6 @@ int	read_file(int fd)
 		ft_putendl_fd("Error\nA malloc failed during parsing", 2);
 		return (1);
 	}
-	return (0);
 }
 
 int	parse_map(int argc, char **argv)
