10d4d732019d8cf824cb841f0049da94fd6b7e55
Jeremy Maia <jmaia@student.42.fr>
2022-07-19 16:22:38 +0200

    Exit if red cross clicked

--- srcs/render/events.c
+++ srcs/render/events.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 14:56:56 by jmaia             #+#    #+#             */
-/*   Updated: 2022/07/19 15:23:21 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/07/19 16:20:45 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 #include "mlx_int.h"
 
 static int	on_key_press(int keycode, void *param);
+static int	on_close_window(void *param);
 
 void	init_events(t_xvar *xvar, t_win_list *win)
 {
+	mlx_hook(win, 33, 1L << 17, &on_close_window, xvar);
 	mlx_key_hook(win, &on_key_press, xvar);
 }
 
@@ -29,5 +31,14 @@ static int	on_key_press(int keycode, void *param)
 	xvar = (t_xvar *) param;
 	if (keycode == XK_Escape)
 		mlx_loop_end(xvar);
+	return (0);
+}
+
+static int	on_close_window(void *param)
+{
+	t_xvar	*xvar;
+
+	xvar = (t_xvar *) param;
+	mlx_loop_end(xvar);
 	return (0);
 }
