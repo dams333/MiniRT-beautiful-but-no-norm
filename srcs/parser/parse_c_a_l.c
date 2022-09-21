94dd3ffa5564670ac0792267d3416f98a6f9fc03
dhubleur <dhubleur@student.42.fr>
2022-09-21 18:20:36 +0200

    style(PARSING): Start norminette
    First norminette pass on some files except for more 25 lines

--- srcs/parser/parse_c_a_l.c
+++ srcs/parser/parse_c_a_l.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:41:06 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/09/21 17:50:57 by marvin           ###   ########.fr       */
+/*   Updated: 2022/09/21 18:19:53 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 bool	parse_camera(char **args, t_parsing *parsing)
 {
-	t_camera_object *obj;
-	
-	if(parsing->camera != NULL)
+	t_camera_object	*obj;
+
+	if (parsing->camera != NULL)
 	{
 		ft_putendl_fd("Error\nThere is more than one camera in the scene", 2);
 		return (false);
 	}
 	if (get_split_size(args) != 4)
 	{
-		ft_putendl_fd("Error\nImpossible to parse camera because argument's count is not 4", 2);
+		ft_putendl_fd("Error\nImpossible to parse camera because argument's", 2);
+		ft_putendl_fd("count is not 4", 2);
 		return (false);
 	}
 	obj = ft_calloc(1, sizeof(t_camera_object));
@@ -32,19 +33,24 @@ bool	parse_camera(char **args, t_parsing *parsing)
 		ft_putendl_fd("Error\nA malloc failed during parsing", 2);
 		return (false);
 	}
-	if (!parse_three_floats(args[1], &(obj->coord_x), &(obj->coord_y), &(obj->coord_z)))
+	if (!parse_three_floats(args[1], &(obj->coord_x), &(obj->coord_y),
+			&(obj->coord_z)))
 	{
 		free(obj);
 		return (false);
 	}
-	if (!parse_three_floats(args[2], &(obj->orientation_x), &(obj->orientation_y), &(obj->orientation_z)))
+	if (!parse_three_floats(args[2], &(obj->orientation_x),
+			&(obj->orientation_y), &(obj->orientation_z)))
 	{
 		free(obj);
 		return (false);
 	}
-	if (obj->orientation_x < -1 || obj->orientation_x > 1 || obj->orientation_y < -1 || obj->orientation_y > 1 || obj->orientation_z < -1 || obj->orientation_z > 1)
+	if (obj->orientation_x < -1 || obj->orientation_x > 1
+		|| obj->orientation_y < -1 || obj->orientation_y > 1
+		|| obj->orientation_z < -1 || obj->orientation_z > 1)
 	{
-		ft_putendl_fd("Error\nImpossible to parse camera because one composant of the orientation vector is not in [-1;1]", 2);
+		ft_putendl_fd("Error\nImpossible to parse camera because one comp", 2);
+		ft_putendl_fd("osant of the orientation vector is not in [-1;1]", 2);
 		free(obj);
 		return (false);
 	}
@@ -55,7 +61,8 @@ bool	parse_camera(char **args, t_parsing *parsing)
 	}
 	if (obj->horizontal_fov < 0 || obj->horizontal_fov > 180)
 	{
-		ft_putendl_fd("Error\nImpossible to parse camera because FOV is not in [0;180]", 2);
+		ft_putendl_fd("Error\nImpossible to parse camera because FOV ", 2);
+		ft_putendl_fd("is not in [0;180]", 2);
 		free(obj);
 		return (false);
 	}
@@ -65,16 +72,18 @@ bool	parse_camera(char **args, t_parsing *parsing)
 
 bool	parse_ambient_lightning(char **args, t_parsing *parsing)
 {
-	t_ambient_lightning_object *obj;
+	t_ambient_lightning_object	*obj;
 
-	if(parsing->ambient_lightning != NULL)
+	if (parsing->ambient_lightning != NULL)
 	{
-		ft_putendl_fd("Error\nThere is more than one ambient lightning in the scene", 2);
+		ft_putendl_fd("Error\nThere is more than one ambient ", 2);
+		ft_putendl_fd("lightning in the scene", 2);
 		return (false);
 	}
 	if (get_split_size(args) != 3)
 	{
-		ft_putendl_fd("Error\nImpossible to parse ambient lightning because argument's count is not 3", 2);
+		ft_putendl_fd("Error\nImpossible to parse ambient light", 2);
+		ft_putendl_fd("ning because argument's count is not 3", 2);
 		return (false);
 	}
 	obj = ft_calloc(1, sizeof(t_ambient_lightning_object));
@@ -90,18 +99,22 @@ bool	parse_ambient_lightning(char **args, t_parsing *parsing)
 	}
 	if (obj->lightning_ratio < 0 || obj->lightning_ratio > 1)
 	{
-		ft_putendl_fd("Error\nImpossible to parse ambient lightning because lightning ration is not in [0;1]", 2);
+		ft_putendl_fd("Error\nImpossible to parse ambient lightning ", 2);
+		ft_putendl_fd("because lightning ration is not in [0;1]", 2);
 		free(obj);
 		return (false);
 	}
-	if (!parse_three_ints(args[2], &(obj->color_r), &(obj->color_g), &(obj->color_b)))
+	if (!parse_three_ints(args[2], &(obj->color_r), &(obj->color_g),
+			&(obj->color_b)))
 	{
 		free(obj);
 		return (false);
 	}
-	if (obj->color_r < 0 || obj->color_r > 255 || obj->color_g < 0 || obj->color_g > 255 || obj->color_b < 0 || obj->color_b > 255)
+	if (obj->color_r < 0 || obj->color_r > 255 || obj->color_g < 0
+		|| obj->color_g > 255 || obj->color_b < 0 || obj->color_b > 255)
 	{
-		ft_putendl_fd("Error\nImpossible to parse ambient lightning because one of the color composant in not in [0;255]", 2);
+		ft_putendl_fd("Error\nImpossible to parse ambient lightning becau", 2);
+		ft_putendl_fd("se one of the color composant in not in [0;255]", 2);
 		free(obj);
 		return (false);
 	}
@@ -111,11 +124,12 @@ bool	parse_ambient_lightning(char **args, t_parsing *parsing)
 
 bool	parse_light(char **args, t_parsing *parsing)
 {
-	t_light_object *obj;
+	t_light_object	*obj;
 
 	if (get_split_size(args) != 4)
 	{
-		ft_putendl_fd("Error\nImpossible to parse light because argument's count is not 4", 2);
+		ft_putendl_fd("Error\nImpossible to parse light because argument's", 2);
+		ft_putendl_fd("count is not 4", 2);
 		return (false);
 	}
 	obj = ft_calloc(1, sizeof(t_light_object));
@@ -124,7 +138,8 @@ bool	parse_light(char **args, t_parsing *parsing)
 		ft_putendl_fd("Error\nA malloc failed during parsing", 2);
 		return (false);
 	}
-	if (!parse_three_floats(args[1], &(obj->coord_x), &(obj->coord_y), &(obj->coord_z)))
+	if (!parse_three_floats(args[1], &(obj->coord_x), &(obj->coord_y),
+			&(obj->coord_z)))
 	{
 		free(obj);
 		return (false);
@@ -136,18 +151,22 @@ bool	parse_light(char **args, t_parsing *parsing)
 	}
 	if (obj->brightness_ratio < 0 || obj->brightness_ratio > 1)
 	{
-		ft_putendl_fd("Error\nImpossible to parse light because brightness ration is not in [0;1]", 2);
+		ft_putendl_fd("Error\nImpossible to parse light because ", 2);
+		ft_putendl_fd("brightness ratio is not in [0;1]", 2);
 		free(obj);
 		return (false);
 	}
-	if (!parse_three_ints(args[3], &(obj->color_r), &(obj->color_g), &(obj->color_b)))
+	if (!parse_three_ints(args[3], &(obj->color_r), &(obj->color_g),
+			&(obj->color_b)))
 	{
 		free(obj);
 		return (false);
 	}
-	if (obj->color_r < 0 || obj->color_r > 255 || obj->color_g < 0 || obj->color_g > 255 || obj->color_b < 0 || obj->color_b > 255)
+	if (obj->color_r < 0 || obj->color_r > 255 || obj->color_g < 0
+		|| obj->color_g > 255 || obj->color_b < 0 || obj->color_b > 255)
 	{
-		ft_putendl_fd("Error\nImpossible to parse light because one of the color composant in not in [0;255]", 2);
+		ft_putendl_fd("Error\nImpossible to parse light because one ", 2);
+		ft_putendl_fd("of the color composant in not in [0;255]", 2);
 		free(obj);
 		return (false);
 	}
