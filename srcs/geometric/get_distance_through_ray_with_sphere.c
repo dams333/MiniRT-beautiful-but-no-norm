9fd3edf8cdf717da6b77758c6f3dcfed7285f23f
Jeremy Maia <jmaia@student.42.fr>
2022-09-22 21:24:49 +0200

    Fix compilation errors

--- srcs/geometric/get_distance_through_ray_with_sphere.c
+++ srcs/geometric/get_distance_through_ray_with_sphere.c
@@ -6,14 +6,16 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 13:25:03 by jmaia             #+#    #+#             */
-/*   Updated: 2022/09/17 15:51:50 by jmaia            ###   ###               */
+/*   Updated: 2022/09/22 21:24:22 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <math.h>
 
 #include "get_intersecting_obj.h"
 #include "sec_degree_utils.h"
 
-static double	get_a(t_ray ray, t_sphere_object *sphere);
+static double	get_a(t_ray ray);
 static double	get_b(t_ray ray, t_sphere_object *sphere);
 static double	get_c(t_ray ray, t_sphere_object *sphere);
 static double	get_nearest_positive_intersection(double a, double b, double c);
@@ -26,7 +28,7 @@ double	get_distance_through_ray_with_sphere(t_ray ray, t_sphere_object *sphere)
 	double	t;
 	t_point	intersection;
 
-	a = get_a(ray, sphere);
+	a = get_a(ray);
 	b = get_b(ray, sphere);
 	c = get_c(ray, sphere);
 	t = get_nearest_positive_intersection(a, b, c);
@@ -38,24 +40,24 @@ double	get_distance_through_ray_with_sphere(t_ray ray, t_sphere_object *sphere)
 		+ pow(intersection.z, 2)));
 }
 
-static double	get_a(t_ray ray, t_sphere_object *sphere)
+static double	get_a(t_ray ray)
 {
 	return (pow(ray.vec.x, 2) + pow(ray.vec.y, 2) + pow(ray.vec.z, 2));
 }
 
 static double	get_b(t_ray ray, t_sphere_object *sphere)
 {
-	return (2 * (ray.vec.x * (ray.point.x - sphere.x)
-		+ ray.vec.y * (ray.point.y - sphere.y)
-		+ ray.vec.z * (ray.point.z - sphere.z)));
+	return (2 * (ray.vec.x * (ray.base.x - sphere->coord_x)
+		+ ray.vec.y * (ray.base.y - sphere->coord_y)
+		+ ray.vec.z * (ray.base.z - sphere->coord_z)));
 }
 
 static double	get_c(t_ray ray, t_sphere_object *sphere)
 {
-	return (pow(ray.point.x - sphere.x, 2)
-		+ pow(ray.point.y - sphere.y, 2)
-		+ pow(ray.point.z - sphere.z 2)
-		- pow(sphere.diameter / 2, 2));
+	return (pow(ray.base.x - sphere->coord_x, 2)
+		+ pow(ray.base.y - sphere->coord_y, 2)
+		+ pow(ray.base.z - sphere->coord_z, 2)
+		- pow(sphere->diameter / 2, 2));
 }
 
 static double	get_nearest_positive_intersection(double a, double b, double c)
