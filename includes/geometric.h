a63569d555d1cfc20ba7cda85ed9d10798b5e461
Jeremy Maia <jmaia@student.42.fr>
2022-09-27 17:56:42 +0200

    Norminetize

--- includes/geometric.h
+++ includes/geometric.h
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 13:35:07 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/09/17 13:02:02 by jmaia            ###   ###               */
+/*   Updated: 2022/09/27 17:52:24 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,19 @@
 
 typedef struct s_point
 {
-    double x;
-    double y;
-    double z;
-}   t_point;
+	double	x;
+	double	y;
+	double	z;
+}	t_point;
 
-double distance(t_point point1, t_point point2);
-
+double	distance(t_point point1, t_point point2);
 
 typedef struct s_vector
 {
-    double		x;
-    double		y;
-    double		z;
-}   t_vector;
+	double		x;
+	double		y;
+	double		z;
+}	t_vector;
 
 typedef struct s_ray
 {
@@ -36,11 +35,11 @@ typedef struct s_ray
 	t_point	vec;
 }	t_ray;
 
-double vector_length(t_vector vector);
-void normalize(t_vector *vector);
-void vector_add(t_vector *vector, t_vector vector_add);
-void vector_substract(t_vector *vector, t_vector vector_substract);
-void multiply_by_scalar(t_vector *vector, double scalar);
-void divide_by_scalar(t_vector *vector, double scalar);
+double	vector_length(t_vector vector);
+void	normalize(t_vector *vector);
+void	vector_add(t_vector *vector, t_vector vector_add);
+void	vector_substract(t_vector *vector, t_vector vector_substract);
+void	multiply_by_scalar(t_vector *vector, double scalar);
+void	divide_by_scalar(t_vector *vector, double scalar);
 
 #endif
--- srcs/geometric/vector.c
+++ srcs/geometric/vector.c
@@ -6,59 +6,45 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 13:36:32 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/06/27 14:41:23 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/09/27 17:51:17 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "geometric.h"
 #include <math.h>
 
-void    from_point(t_vector *vector, t_point point1, t_point point2)
+void	from_point(t_vector *vector, t_point point1, t_point point2)
 {
-    vector->x = point2.x - point1.x;
-    vector->y = point2.y - point1.y;
-    vector->z = point2.z - point1.z;
+	vector->x = point2.x - point1.x;
+	vector->y = point2.y - point1.y;
+	vector->z = point2.z - point1.z;
 }
 
-double vector_length(t_vector vector)
+double	vector_length(t_vector vector)
 {
-    return (pow(vector.x, 2) + pow(vector.y, 2) + pow(vector.z, 2));
+	return (pow(vector.x, 2) + pow(vector.y, 2) + pow(vector.z, 2));
 }
 
-void normalize(t_vector *vector)
+void	normalize(t_vector *vector)
 {
-    double length;
+	double	length;
 
-    length = vector_length(*vector);
-    vector->x /= length;
-    vector->y /= length;
-    vector->z /= length;
+	length = vector_length(*vector);
+	vector->x /= length;
+	vector->y /= length;
+	vector->z /= length;
 }
 
-void vector_add(t_vector *vector, t_vector vector_add)
+void	vector_add(t_vector *vector, t_vector vector_add)
 {
-    vector->x += vector_add.x;
-    vector->y += vector_add.y;
-    vector->z += vector_add.z;
+	vector->x += vector_add.x;
+	vector->y += vector_add.y;
+	vector->z += vector_add.z;
 }
 
-void vector_substract(t_vector *vector, t_vector vector_substract)
+void	vector_substract(t_vector *vector, t_vector vector_substract)
 {
-    vector->x -= vector_substract.x;
-    vector->y -= vector_substract.y;
-    vector->z -= vector_substract.z;
-}
-
-void multiply_by_scalar(t_vector *vector, double scalar)
-{
-    vector->x *= scalar;
-    vector->y *= scalar;
-    vector->z *= scalar;
-}
-
-void divide_by_scalar(t_vector *vector, double scalar)
-{
-    vector->x /= scalar;
-    vector->y /= scalar;
-    vector->z /= scalar;
+	vector->x -= vector_substract.x;
+	vector->y -= vector_substract.y;
+	vector->z -= vector_substract.z;
 }
