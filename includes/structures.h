cea41e3b9d672d51ace948ca03373d66da22f5f1
Jeremy Maia <jmaia@student.42.fr>
2022-10-10 14:10:44 +0200

    Change objects structures in parsing

--- includes/structures.h
+++ includes/structures.h
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 11:43:56 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/10/10 13:57:12 by jmaia            ###   ###               */
+/*   Updated: 2022/10/10 14:04:14 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,9 @@ typedef struct s_sphere
 {
 	t_point	pos;
 	double	diameter;
-	int		color;
+	int		color_r;
+	int		color_g;
+	int		color_b;
 }	t_sphere;
 
 typedef struct s_plane_object
@@ -99,7 +101,9 @@ typedef struct s_plane
 {
 	t_vector	orientation;
 	double		d;
-	int			color;
+	int			color_r;
+	int			color_g;
+	int			color_b;
 }	t_plane;
 
 typedef struct s_cylinder_object
@@ -123,14 +127,18 @@ typedef struct s_cylinder
 	t_vector	orientation;
 	double		diameter;
 	double		height;
-	int			color;
+	int			color_r;
+	int			color_g;
+	int			color_b;
 }	t_cylinder;
 
 typedef struct s_ellipsoid
 {
 	t_point					pos;
 	t_sec_degree_factors	factors;
-	int						color;
+	int						color_r;
+	int						color_g;
+	int						color_b;
 }	t_ellipsoid;
 
 typedef struct s_parsing
--- srcs/parser/parse_sp_pl.c
+++ srcs/parser/parse_sp_pl.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 10:26:02 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/10/10 13:58:25 by jmaia            ###   ###               */
+/*   Updated: 2022/10/10 14:07:42 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ one composant of the orientation vector is not in [-1;1]"
 #define PLANE_ERROR_COLOR "Error\nImpossible to parse plane \
 because one of the color composant is not in [0;255]"
 
-bool	parse_sphere_2(char **args, t_parsing *parsing, t_sphere_object *obj)
+bool	parse_sphere_2(char **args, t_parsing *parsing, t_sphere *obj)
 {
 	if (!parse_three_ints(args[3], &(obj->color_r), &(obj->color_g),
 			&(obj->color_b)))
@@ -43,15 +43,15 @@ bool	parse_sphere_2(char **args, t_parsing *parsing, t_sphere_object *obj)
 
 bool	parse_sphere(char **args, t_parsing *parsing)
 {
-	t_sphere_object	*obj;
+	t_sphere	*obj;
 
 	if (get_split_size(args) != 4)
 		return (ft_putendl_fd(SPHERE_ERROR_ARGS, 2), false);
-	obj = ft_calloc(1, sizeof(t_sphere_object));
+	obj = ft_calloc(1, sizeof(t_sphere));
 	if (!obj)
 		return (ft_putendl_fd(MALLOC_ERROR, 2), false);
-	if (!parse_three_doubles(args[1], &(obj->coord_x), &(obj->coord_y),
-			&(obj->coord_z)))
+	if (!parse_three_doubles(args[1], &(obj->pos.x), &(obj->pos.y),
+			&(obj->pos.z)))
 		return (free(obj), false);
 	if (!parse_double(args[2], &(obj->diameter)))
 		return (free(obj), false);
@@ -63,10 +63,11 @@ bool	parse_sphere(char **args, t_parsing *parsing)
 	return (parse_sphere_2(args, parsing, obj));
 }
 
-bool	parse_plane_2(char **args, t_parsing *parsing, t_plane_object *obj)
+bool	parse_plane_2(char **args, t_parsing *parsing, t_plane *obj,
+			t_point pos)
 {
-	obj->d = -obj->orientation_x * obj->coord_x - obj->orientation_y
-		* obj->coord_y - obj->orientation_z * obj->coord_z;
+	obj->d = -obj->orientation.x * pos.x - obj->orientation.y
+		* pos.y - obj->orientation.z * pos.z;
 	if (!parse_three_ints(args[3], &(obj->color_r), &(obj->color_g),
 			&(obj->color_b)))
 		return (free(obj), false);
@@ -83,25 +84,26 @@ bool	parse_plane_2(char **args, t_parsing *parsing, t_plane_object *obj)
 
 bool	parse_plane(char **args, t_parsing *parsing)
 {
-	t_plane_object	*obj;
+	t_plane	*obj;
+	t_point	point;
 
 	if (get_split_size(args) != 4)
 		return (ft_putendl_fd(PLANE_ERROR_ARGS, 2), false);
-	obj = ft_calloc(1, sizeof(t_plane_object));
+	obj = ft_calloc(1, sizeof(t_plane));
 	if (!obj)
 		return (ft_putendl_fd(MALLOC_ERROR, 2), false);
-	if (!parse_three_doubles(args[1], &(obj->coord_x), &(obj->coord_y),
-			&(obj->coord_z)))
+	if (!parse_three_doubles(args[1], &(point.x), &(point.y),
+			&(point.z)))
 		return (free(obj), false);
-	if (!parse_three_doubles(args[2], &(obj->orientation_x),
-			&(obj->orientation_y), &(obj->orientation_z)))
+	if (!parse_three_doubles(args[2], &(obj->orientation.x),
+			&(obj->orientation.y), &(obj->orientation.z)))
 		return (free(obj), false);
-	if (obj->orientation_x < -1 || obj->orientation_x > 1
-		|| obj->orientation_y < -1 || obj->orientation_y > 1
-		|| obj->orientation_z < -1 || obj->orientation_z > 1)
+	if (obj->orientation.x < -1 || obj->orientation.x > 1
+		|| obj->orientation.y < -1 || obj->orientation.y > 1
+		|| obj->orientation.z < -1 || obj->orientation.z > 1)
 	{
 		free(obj);
 		return (ft_putendl_fd(PLANE_ERROR_ORIENTATION, 2), false);
 	}
-	return (parse_plane_2(args, parsing, obj));
+	return (parse_plane_2(args, parsing, obj, point));
 }
