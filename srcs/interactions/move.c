1e1ab16e1180472baf08a4c54d073aeb39120a9e
dhubleur <dhubleur@student.42.fr>
2022-10-11 14:03:33 +0200

    style(INTERACTIONS): Norm move function

--- srcs/interactions/move.c
+++ srcs/interactions/move.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:41:53 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/10/11 13:57:09 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/10/11 14:03:03 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,15 +37,8 @@ void	extract_coord(t_params *params, double *coords[3])
 	}
 }
 
-void	move(int keycode, t_params *params)
+void	exec_move(int keycode, t_vector ori, double *coords[3])
 {
-	t_vector	ori;
-	double		*coords[3];
-
-	ori = (t_vector){params->parsing->camera->orientation_x,
-		0, params->parsing->camera->orientation_z};
-	normalize(&ori);
-	extract_coord(params, coords);
 	if (keycode == XK_w)
 	{
 		*(coords[0]) += ori.x * 0.1;
@@ -70,4 +63,16 @@ void	move(int keycode, t_params *params)
 		*(coords[1]) -= 0.1;
 	else if (keycode == XK_Shift_L)
 		*(coords[1]) += 0.1;
+}
+
+void	move(int keycode, t_params *params)
+{
+	t_vector	ori;
+	double		*coords[3];
+
+	ori = (t_vector){params->parsing->camera->orientation_x,
+		0, params->parsing->camera->orientation_z};
+	normalize(&ori);
+	extract_coord(params, coords);
+	exec_move(keycode, ori, coords);
 }
