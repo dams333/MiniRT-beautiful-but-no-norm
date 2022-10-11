61acf3a03731f99c0eaadd6267c564eb62b6feb7
Jeremy Maia <jmaia@student.42.fr>
2022-10-11 17:53:29 +0200

    Center world to sphere

--- includes/checkerboard.h
+++ includes/checkerboard.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:16:28 by jmaia             #+#    #+#             */
-/*   Updated: 2022/10/11 12:09:55 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/10/11 17:49:24 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ typedef struct s_checkerboard
 }	t_checkerboard;
 
 int			is_filled(t_checkerboard checkerboard, double x, double y);
-int			is_sp_filled(t_checkerboard checkerboard, t_point point);
+int			is_sp_filled(t_checkerboard checkerboard, t_point point,
+				t_point center);
 t_point2d	to_checkerboard_pos(t_point point);
 
 #endif
