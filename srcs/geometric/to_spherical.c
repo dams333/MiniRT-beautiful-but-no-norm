19f967f3ee38743f912a6255ebc7751a576d45f2
Jeremy Maia <jmaia@student.42.fr>
2022-10-11 17:53:40 +0200

    Offset inclination of spherical coordonate

--- srcs/geometric/to_spherical.c
+++ srcs/geometric/to_spherical.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 10:39:22 by jmaia             #+#    #+#             */
-/*   Updated: 2022/10/11 10:55:18 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/10/11 17:51:49 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,19 @@ t_spherical_point	to_spherical(t_point point)
 
 static double	get_inclination(t_point point)
 {
+	double	inclination;
+
+	inclination = NAN;
 	if (point.x > 0)
-		return (atan(point.y / point.x));
+		inclination = atan(point.y / point.x);
 	if (point.x < 0 && point.y >= 0)
-		return (atan(point.y / point.x) + M_PI);
+		inclination = atan(point.y / point.x) + M_PI;
 	if (point.x < 0 && point.y < 0)
-		return (atan(point.y / point.x) - M_PI);
+		inclination = atan(point.y / point.x) - M_PI;
 	if (point.x == 0 && point.y > 0)
-		return (M_PI_2);
+		inclination = M_PI_2;
 	if (point.x == 0 && point.y < 0)
-		return (-M_PI_2);
-	return (NAN);
+		inclination = -M_PI_2;
+	inclination += M_PI;
+	return (inclination);
 }
