18d9851b5344adebc8d314bf39c0961aebd4dc50
Jeremy Maia <jmaia@student.42.fr>
2022-10-12 12:36:51 +0200

    Norminetize

--- srcs/checkerboard/to_checkerboard_pos.c
+++ srcs/checkerboard/to_checkerboard_pos.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:57:42 by jmaia             #+#    #+#             */
-/*   Updated: 2022/10/12 12:17:51 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/10/12 12:36:35 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 #include "checkerboard.h"
 
-//static t_point2d	to_checkerboard_pos_from_cylinder(t_point point);
-//static t_point2d	to_checkerboard_pos_from_ellipsoid(t_point point);
-//static t_point2d	to_checkerboard_pos_from_plane(t_point point);
 static t_point2d	to_checkerboard_pos_from_sphere(t_point point);
 
 t_point2d	to_checkerboard_pos(t_generic_object *obj, t_point intersection)
@@ -25,12 +22,6 @@ t_point2d	to_checkerboard_pos(t_generic_object *obj, t_point intersection)
 
 	if (obj->type == SPHERE)
 		board_pos = to_checkerboard_pos_from_sphere(intersection);
-//	else if (obj->type == CYLINDER)
-//		board_pos = to_checkerboard_pos_from_cylinder(intersection);
-//	else if (obj->type == PLANE)
-//		board_pos = to_checkerboard_pos_from_plane(intersection);
-//	else if (obj->type == ELLIPSOID)
-//		board_pos = to_checkerboard_pos_from_ellipsoid(intersection);
 	return (board_pos);
 }
 
