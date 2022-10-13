99e1b7530e4bae903cd8144e22f8cd339a2fd09c
Jeremy Maia <jmaia@student.42.fr>
2022-10-13 19:28:01 +0200

    Fix sphere elsewhere than origin

--- srcs/checkerboard/to_checkerboard_pos.c
+++ srcs/checkerboard/to_checkerboard_pos.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:57:42 by jmaia             #+#    #+#             */
-/*   Updated: 2022/10/12 12:36:35 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/10/13 19:21:05 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,26 @@
 
 #include "checkerboard.h"
 
-static t_point2d	to_checkerboard_pos_from_sphere(t_point point);
+static t_point2d	to_checkerboard_pos_from_sphere(t_point center,
+						t_point point);
 
 t_point2d	to_checkerboard_pos(t_generic_object *obj, t_point intersection)
 {
 	t_point2d	board_pos;
 
 	if (obj->type == SPHERE)
-		board_pos = to_checkerboard_pos_from_sphere(intersection);
+		board_pos = to_checkerboard_pos_from_sphere(
+				((t_sphere *)obj->specific_object)->pos, intersection);
 	return (board_pos);
 }
 
-static t_point2d	to_checkerboard_pos_from_sphere(t_point point)
+static t_point2d	to_checkerboard_pos_from_sphere(t_point center,
+						t_point point)
 {
 	t_spherical_point	sp_point;
 	t_point2d			board_point;
 
+	vector_substract(&point, point, center);
 	sp_point = to_spherical(point);
 	board_point.x = sp_point.azimuth / (2 * M_PI);
 	board_point.y = sp_point.inclination / M_PI;
