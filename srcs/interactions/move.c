191b44503b24e48d8e980e58ddd1e2f1917cbd2f
dhubleur <dhubleur@student.42.fr>
2022-10-30 11:53:04 +0100

    fix(INTERACTIONS): Repair move
    The move interaction only move on axi and doesnt use the orientation

--- srcs/interactions/move.c
+++ srcs/interactions/move.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:41:53 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/10/11 15:29:38 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/10/30 11:52:21 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,28 +47,16 @@ void	extract_coord(t_params *params, double *coords[3])
 		extract_obj_coord(params, coords);
 }
 
-void	exec_move(int keycode, t_vector ori, double *coords[3])
+void	exec_move(int keycode, double *coords[3])
 {
 	if (keycode == XK_w)
-	{
-		*(coords[0]) += ori.x * 0.1;
-		*(coords[2]) += ori.z * 0.1;
-	}
+		*(coords[2]) += 0.1;
 	else if (keycode == XK_s)
-	{
-		*(coords[0]) -= ori.x * 0.1;
-		*(coords[2]) -= ori.z * 0.1;
-	}
+		*(coords[2]) -= 0.1;
 	else if (keycode == XK_d)
-	{
-		*(coords[0]) += ori.z * 0.1;
-		*(coords[2]) += ori.x * 0.1;
-	}
+		*(coords[0]) += 0.1;
 	else if (keycode == XK_a)
-	{
-		*(coords[0]) -= ori.z * 0.1;
-		*(coords[2]) -= ori.x * 0.1;
-	}
+		*(coords[0]) -= 0.1;
 	else if (keycode == XK_space)
 		*(coords[1]) -= 0.1;
 	else if (keycode == XK_Shift_L)
@@ -77,7 +65,6 @@ void	exec_move(int keycode, t_vector ori, double *coords[3])
 
 void	move(int keycode, t_params *params)
 {
-	t_vector	ori;
 	double		*coords[3];
 	double		tmp;
 
@@ -85,9 +72,6 @@ void	move(int keycode, t_params *params)
 	coords[0] = &tmp;
 	coords[1] = &tmp;
 	coords[2] = &tmp;
-	ori = (t_vector){params->parsing->camera->orientation_x,
-		0, params->parsing->camera->orientation_z};
-	normalize(&ori);
 	extract_coord(params, coords);
-	exec_move(keycode, ori, coords);
+	exec_move(keycode, coords);
 }
