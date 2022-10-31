3a3444f1b1a684e22da01523d518923e0289a452
dhubleur <dhubleur@student.42.fr>
2022-10-31 17:21:39 +0100

    fix(PARSER): Remove unused message

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 17:44:43 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/10/30 12:04:30 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/10/31 17:21:13 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,7 @@ bool	read_file(int fd, t_parsing *parsing)
 	if (res == READ_ERROR)
 		return (ft_putendl_fd("Error\nThe file cannot be read", 2), false);
 	else if (res == END_OF_READ)
-		return (ft_putendl_fd("matrix_multsing !", 1), true);
+		return (true);
 	else
 		return (ft_putendl_fd("Error\nA malloc failed during parsing", 2),
 			false);
